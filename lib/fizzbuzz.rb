57a163fca6a707fd1a9c28d27972bc8449b1d348
Alex <avlonitis@msn.com>
2016-01-12 20:05:44 +0000

    fizzbuzz with Arnold

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,30 @@
+class Integer
+
+	def fizzbuzz
+		number = self
+		case
+			#when number =~ (/[a-zA-Z]/) then "no letters allowed" ------#before refactoring, not for integer class
+			#when !(number.is_a?(Integer)) then "Only numbers allowed" ------#before refactoring, not for integer class
+			when number % 3 == 0 && number % 5 == 0 then 'fizzbuzz'
+			when number % 3 == 0 then 'fizz'
+			when number % 5 == 0 then 'buzz'
+		else
+			number
+		end
+	end
+
+end
+
+=begin
 def fizzbuzz(number)
-	if number % 3 == 0 || number % 5 == 0
-		return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
-		return 'fizz' if number % 3 == 0
-		return 'buzz' if number % 5 == 0
+	case
+		when number =~ (/[a-zA-Z]/) then "no letters allowed"
+		when !(number.is_a?(Integer)) then "Only numbers allowed" 
+		when number % 3 == 0 && number % 5 == 0 then 'fizzbuzz'
+		when number % 3 == 0 then 'fizz'
+		when number % 5 == 0 then 'buzz'
 	else
-		return number
+		number
 	end
 end
+=end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/fizzbuzz.rb'
 
 describe 'fizzbuzz' do
+=begin
 	it 'returns "fizz" when passed 3' do
 		expect(fizzbuzz(3)).to eq 'fizz'
 	end
@@ -13,4 +14,38 @@ describe 'fizzbuzz' do
 	it 'returns "8" when passed 8' do
 		expect(fizzbuzz(8)).to eq 8
 	end
+	it 'returns "fizz" when passed a multiple of 3' do
+		[3,6,9,12,18,21].each do |multiple_3|
+			expect(fizzbuzz(multiple_3)).to eq 'fizz'
+		end
+	end
+	it 'returns "no letters allowed" when passed a letter' do
+		("a".."z").each do |letter|
+			expect(fizzbuzz(letter)).to eq 'no letters allowed'
+		end
+	end
+=end
+	it 'returns "fizz" when passed 3' do
+		expect(3.fizzbuzz).to eq 'fizz'
+	end
+	it 'returns "buzz" when passed 5' do
+		expect(5.fizzbuzz).to eq 'buzz'
+	end
+	it 'returns "fizzbuzz" when passed 15' do
+		expect(15.fizzbuzz).to eq 'fizzbuzz'
+	end
+	it 'returns "8" when passed 8' do
+		expect(8.fizzbuzz).to eq 8
+	end
+	it 'returns "fizz" when passed a multiple of 3' do
+		[3,6,9,12,18,21].each do |multiple_3|
+			expect(multiple_3.fizzbuzz).to eq 'fizz'
+		end
+	end
+	it 'returns "no letters allowed" when passed a letter' do
+		("a".."z").each do |letter|
+			expect(letter.respond_to?(:fizzbuzz)).to eq false
+		end
+	end
+
 end
